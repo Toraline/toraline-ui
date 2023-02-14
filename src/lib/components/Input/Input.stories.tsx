a796225c63540424691f7de014e0c831c69fd967
Caroline <ambar.caroline@gmail.com>
2023-02-14 09:58:57 -0300

    Add Input Component

---
+++ src/lib/components/Input/Input.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import  Input from './Input';
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Input',
+  component: Input,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    
+  },
+} as ComponentMeta<typeof Input>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Input> = () => <Input name='title' id='title' label='Title' />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {};
